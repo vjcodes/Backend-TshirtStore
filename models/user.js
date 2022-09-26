d0a33ff857b6bbbaf053915b7be6271d01ce37cf
Vinayak Jaiswal <vinayak.kris@gmail.com>
2022-09-26 22:06:31 +0530

    -created schema methods for validating password and creating jwt token

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -45,12 +46,29 @@ const userSchema = new mongoose.Schema({
 
 })
 
-//encrypt password before save
+//encrypt password before save - HOOKS
 userSchema.pre('save', async function(next) {
     if( !this.isModified('password')) {
         return next();
     }
     this.password = await bcrypt.hash(this.password, 10)
 });
+
+// validate the password with passed on user password
+userSchema.methods.IsValidatedPassword = async function(usersendPassword){
+    return await bcrypt.compare(usersendPassword, this.password)
+};
+
+//create and return jwt token
+userSchema.methods.getJwtToken = function(){
+    return jwt.sign({
+        id: this._id
+    },
+    process.env.JWT_SECRET,
+    {
+        expiresIn: process.env.JWT_EXPIRY
+    }
+    );
+}
 
 module.exports = mongoose.model('User', userSchema)
