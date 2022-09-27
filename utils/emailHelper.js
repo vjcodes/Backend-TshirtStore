26a8ff5920626b128580ed1ef42449e060b8aac8
vjcodes <47989148+vjcodes@users.noreply.github.com>
2022-09-27 18:41:47 +0530

    - setup nodemailer in project under utils (#14)

---
+++ utils/emailHelper.js
@@ -0,0 +1,25 @@
+const nodemailer = require("nodemailer");
+
+const mailHelper = async (option) => {
+  // create reusable transporter object using the default SMTP transport
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS
+    }
+  });
+
+  const message = {
+    from: 'vinayak@lco.dev', // sender address
+    to: option.email, // list of receivers
+    subject: option.subject, // Subject line
+    text: option.message, // plain text body
+  }
+
+  // send mail with defined transport object
+  await transporter.sendMail(message);
+}
+
+module.exports = mailHelper
