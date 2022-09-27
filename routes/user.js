dff62021c3d192d0a95ab7f09174ac58a1858059
vjcodes <47989148+vjcodes@users.noreply.github.com>
2022-09-27 17:28:12 +0530

    -implemented user logout route and controller (#13)

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { signup, login } = require('../controllers/userController');
+const { signup, login, logout } = require('../controllers/userController');
 const router = express.Router()
 
 router.route('/signup').post(signup);
 router.route('/login').post(login)
+router.route('/logout').get(logout)
 
 
 module.exports = router;
