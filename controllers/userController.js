14c4ceb5ead4d6a16fd3093ee368175b618c8c2a
vjcodes <47989148+vjcodes@users.noreply.github.com>
2022-09-27 15:03:33 +0530

    - handled image upload using cloudinary in signup (#10)
    - tested using temporary template using ejs

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,8 +2,23 @@ const User = require('../models/user')
 const BigPromise = require("../middlewares/bigPromise");
 const CustomError = require('../utils/customError');
 const cookieToken = require('../utils/cookieToken');
+const fileUpload = require('express-fileupload')
+const cloudinary = require('cloudinary')
 
 exports.signup = BigPromise(async (req, res, next) => {
+
+    let result;
+
+    if(req.files) {
+        let file = req.files.photo
+        result = await cloudinary.v2.uploader.upload(file.tempFilePath, {
+            folder: "users",
+            width: 150,
+            crop: "scale"
+        })
+    }
+
+
     const {name, email, password} = req.body
 
     if(!email || !name || !password){
@@ -16,7 +31,11 @@ exports.signup = BigPromise(async (req, res, next) => {
     const user = await User.create({
         name,
         email,
-        password
+        password,
+        photo: {
+            id: result.public_id,
+            secure_url: result.secure_url
+        }
     })
 
     cookieToken(user, res)
