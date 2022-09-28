9a2c1f30e1e5e0f8f27cde0078b718c17c90666b
Vinayak Jaiswal <vinayak.kris@gmail.com>
2022-09-28 13:12:36 +0530

    -created reset pwd route and controller

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const { signup, login, logout, forgotPassword } = require('../controllers/userController');
+const { signup, login, logout, forgotPassword, passwordReset } = require('../controllers/userController');
 const router = express.Router()
 
 router.route('/signup').post(signup);
 router.route('/login').post(login)
 router.route('/logout').get(logout)
 router.route('/forgotPassword').post(forgotPassword)
+router.route("/password/reset/:token").post(passwordReset)
 
 
 module.exports = router;
