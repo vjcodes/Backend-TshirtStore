6c920619a560d0a6b22a11a4426ae1389c23a14c
vjcodes <47989148+vjcodes@users.noreply.github.com>
2022-09-28 13:53:01 +0530

    - created user dashboard controller and route (#18)

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const { signup, login, logout, forgotPassword, passwordReset } = require('../controllers/userController');
+const { signup, login, logout, forgotPassword, passwordReset, getLoggedInUserDetails } = require('../controllers/userController');
 const router = express.Router()
+const { isLoggedIn } = require("../middlewares/user")
 
 router.route('/signup').post(signup);
 router.route('/login').post(login)
 router.route('/logout').get(logout)
 router.route('/forgotPassword').post(forgotPassword)
 router.route("/password/reset/:token").post(passwordReset)
+router.route("/userdashboard").get(isLoggedIn, getLoggedInUserDetails)
 
 
 module.exports = router;
