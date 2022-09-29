5d0f77373ec6c16dea845b62e057b2e53fd0bdc7
vjcodes <47989148+vjcodes@users.noreply.github.com>
2022-09-29 14:44:24 +0530

    - created add product controller (#25)

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,7 @@
+const Product = require('../models/product');
 const BigPromise = require("../middlewares/bigPromise");
+const CustomError = require('../utils/customError');
+const cloudinary = require('cloudinary');
 
 exports.testProduct = BigPromise(async(req,res) => {
     // const db = await something()
@@ -6,4 +9,40 @@ exports.testProduct = BigPromise(async(req,res) => {
         success: true,
         greeting: "this is test for product"
     });
-})
+});
+
+exports.addProduct = BigPromise(async(req, res, next) => {
+   // images
+
+    let imageArray = [];
+
+    if(!req.files) {
+        return next(new CustomError('images are required', 401))
+    }
+
+    if(req.files){
+        for (let index = 0; index < req.files.photos.length; index++) {
+            let result = await cloudinary.v2.uploader.upload(req.files.photos[index].
+                tempFilePath, {
+                    folder: "products"
+                })
+            
+            imageArray.push({
+                id: result.public_id,
+                secure_url: result.secure_url
+            })
+            
+        }
+    }
+
+    req.body.photos = imageArray
+    req.body.user = req.user.id
+
+    const product = await Product.create(req.body)
+
+    res.status(200).json({
+        success: true,
+        product
+    })
+
+});
